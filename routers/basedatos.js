bec6f5ad252ac125102d14eef884153eba4c5dc4
kiin The Dovah <kiinthedovah@gmail.com>
2020-09-20 20:16:14 -0500

    Update basedatos.js

--- routers/basedatos.js
+++ routers/basedatos.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 const Router = require("express-promise-router");
 
 const pool = new Pool({
-  user: "fredyb",
-  host: "localhost",
-  database: "postgres",
-  password: "univalle",
+  user: "xnilnafysbemkw",
+  host: "ec2-184-73-249-9.compute-1.amazonaws.com",
+  database: "d2g2hsl8s8oq35",
+  password: "9b29cd206aa2fb18a01a2377af9c3b5000fdb6c91b86ddee3c10542fd9c58717",
   port: 5432,
 });
 
