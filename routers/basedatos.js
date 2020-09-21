b2c769b47be8485aa5f89b3fcfdbf936b46b35c3
kiin The Dovah <kiinthedovah@gmail.com>
2020-09-20 20:28:50 -0500

    asd

--- routers/basedatos.js
+++ routers/basedatos.js
@@ -3,7 +3,7 @@ const Router = require("express-promise-router");
 
 const pool = new Pool({
   user: "xnilnafysbemkw",
-  host: "ec2-184-73-249-9.compute-1.amazonaws.com",
+  host: "https://testdbuv.herokuapp.com",
   database: "d2g2hsl8s8oq35",
   password: "9b29cd206aa2fb18a01a2377af9c3b5000fdb6c91b86ddee3c10542fd9c58717",
   port: 5432,
